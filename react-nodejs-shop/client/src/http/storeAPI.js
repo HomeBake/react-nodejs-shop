6804c1048d848d85522f07f961255ee3fcd6c92d
homebake <oleg.kalyugin2001@mail.ru>
2022-07-08 22:26:35 +0200

    Сократил ссылки

--- react-nodejs-shop/client/src/http/storeAPI.js
+++ react-nodejs-shop/client/src/http/storeAPI.js
@@ -1,8 +1,9 @@
 import {$authHost, $host} from "./index";
 
+const baseURL = process.env.REACT_APP_API_URL + process.env.REACT_APP_API_PREFIX
 
 export const fetchDevices = async (typeId, brandId, search, limit, page) => {
-    const url = new URL(process.env.REACT_APP_API_URL + process.env.REACT_APP_API_PREFIX + process.env.REACT_APP_API_STORE_URL)
+    const url = new URL(baseURL + process.env.REACT_APP_API_STORE_URL)
     typeId && url.searchParams.append("typeId",typeId)
     brandId && url.searchParams.append("brandId",brandId)
     search && url.searchParams.append("search",search)
@@ -13,13 +14,30 @@ export const fetchDevices = async (typeId, brandId, search, limit, page) => {
 }
 
 export const fetchTypes = async () => {
-    const url = new URL(process.env.REACT_APP_API_URL + process.env.REACT_APP_API_PREFIX + process.env.REACT_APP_API_TYPE_URL)
+    const url = new URL(baseURL + process.env.REACT_APP_API_TYPE_URL)
     const {data} = await $host.get(url)
     return data
 }
 
 export const fetchBrands = async () => {
-    const url = new URL(process.env.REACT_APP_API_URL + process.env.REACT_APP_API_PREFIX + process.env.REACT_APP_API_BRAND_URL)
+    const url = new URL(baseURL + process.env.REACT_APP_API_BRAND_URL)
     const {data} = await $host.get(url)
     return data
+}
+
+export const addBrand = async (brandTitle) => {
+    const url = new URL(baseURL + process.env.REACT_APP_API_BRAND_URL + 'add')
+    const data = await $authHost.post(url, {name: brandTitle})
+    return data
+}
+
+export const addType = async (typeTitle) => {
+    const url = new URL(baseURL + process.env.REACT_APP_API_TYPE_URL + 'add')
+    const data = await $authHost.post(url, {name: typeTitle})
+    return data
+}
+
+export const addDevice = async () => {
+    const url = new URL(baseURL + process.env.REACT_APP_API_BRAND_URL + 'add')
+
 }
